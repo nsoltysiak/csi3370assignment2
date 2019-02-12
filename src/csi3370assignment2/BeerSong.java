07287210d3a198068b5272e6f55de6813740243c
Nick <nsoltysiak@gmail.com>
2019-02-11 19:26:19 -0500

    Added functionality to main function and Ninety_Nine_Bottles_of_Beer function.

--- src/csi3370assignment2/BeerSong.java
+++ src/csi3370assignment2/BeerSong.java
@@ -5,11 +5,31 @@ public class BeerSong
     
     public static void main(String[] args) 
     {
-    
+        Ninety_Nine_Bottles_of_Beer();
+        
     }
     
     public static void Ninety_Nine_Bottles_of_Beer()
     {
+       for (int i = 99; i > 2; i--)
+        {     
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
+            System.out.println("take one down, pass it around, " + (i - 1) + " bottles of beer on the wall.");
+            System.out.println();
+        }
         
+        for (int i = 2; i > 1; i--)
+        {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
+            System.out.println("take one down, pass it around, " + (i - 1) + " bottle of beer on the wall.");
+            System.out.println();
+        }
+        
+        for (int i = 1; i > 0; i--)
+        {
+            System.out.println(i + " bottle of beer on the wall, " + i + " bottle of beer");
+            System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
+            System.out.println();
+        } 
     }
 }
